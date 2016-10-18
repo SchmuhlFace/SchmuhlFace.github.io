79a0ab8afa131d7e592b7fd0c90adc2b78898b0b
SchmuhlFace <eschmuhl@gmail.com>
2016-10-18 10:33:41 -0400

    Add js file and clickable red border

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("THe script is running!");
+
+function addRedBorder(event) {
+  image.style.border = "4px solid red";
+}
+
+var image = document.getElementById("face");
+image.addEventListener("click", addRedBorder);
