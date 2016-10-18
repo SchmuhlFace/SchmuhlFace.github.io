4189a3986aad2571a87664cad59a56c4fd35554a
SchmuhlFace <eschmuhl@gmail.com>
2016-10-18 10:41:36 -0400

    readd event to js and redo release 2

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 console.log("THe script is running!");
 
-function addRedBorder() {
+function addRedBorder(event) {
   image.style.border = "4px solid red";
 }
 
 var image = document.getElementById("face");
-// image.addEventListener("click", addRedBorder);
-addRedBorder();
+image.addEventListener("click", addRedBorder);
